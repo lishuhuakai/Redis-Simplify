2700b6d20be19fb466d2cc762879949ae69f9d64
lishuhuakai <lishuhuakai@gmail.com>
2016-11-27 14:47:36 +0800

    finished the code of string commands!

---
+++ 03-finished_string_command/anet.c
@@ -0,0 +1,190 @@
+#include "fmacros.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <netdb.h>
+#include "anet.h"
+
+/*
+* 打印错误信息
+*/
+static void anetSetError(char *err, const char *fmt, ...) {
+	va_list ap;
+	if (!err) return;
+	va_start(ap, fmt);
+	vsnprintf(err, ANET_ERR_LEN, fmt, ap);
+	va_end(ap);
+}
+
+/*
+* 设置地址为可重用
+*/
+static int anetSetReuseAddr(char *err, int fd) {
+	int yes = 1;
+	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+		anetSetError(err, "setsocketopt SO_REUSEADDR: %s", strerror(errno));
+		return ANET_ERR;
+	}
+	return ANET_OK;
+}
+
+/*
+* 绑定并创建监听套接字
+*/
+static int anetListen(char *err, int fd, struct sockaddr *sa, socklen_t len, int backlog) {
+	if (bind(fd, sa, len) == -1) {
+		anetSetError(err, "bind: %s", strerror(errno));
+		close(fd);
+		return ANET_ERR;
+	}
+
+	if (listen(fd, backlog) == -1) {
+		anetSetError(err, "listen: %s", strerror(errno));
+		close(fd);
+		return ANET_ERR;
+	}
+
+	return ANET_OK;
+}
+
+
+static int anetGenericAccept(char *err, int s, struct sockaddr *sa, socklen_t *len) {
+	int fd;
+	while (1) {
+		fd = accept(s, sa, len);
+		if (fd == -1) {
+			if (errno == EINTR)
+				continue;
+			else {
+				anetSetError(err, "accept: %s", strerror(errno));
+				return ANET_ERR;
+			}
+		}
+		break;
+	}
+	return fd;
+}
+
+
+/*
+* TCP 连接 accept 函数
+*/
+int anetTcpAccept(char *err, int s, char *ip, size_t ip_len, int *port) {
+	int fd;
+	struct sockaddr_storage sa;
+	socklen_t salen = sizeof(sa);
+	if ((fd = anetGenericAccept(err, s, (struct sockaddr*)&sa, &salen)) == -1)
+		return ANET_ERR;
+
+	if (sa.ss_family == AF_INET) {
+		struct sockaddr_in *s = (struct sockaddr_in *)&sa;
+		if (ip) inet_ntop(AF_INET, (void*)&(s->sin_addr), ip, ip_len);
+		if (port) *port = ntohs(s->sin_port);
+	}
+	else {
+		struct sockaddr_in6 *s = (struct sockaddr_in6 *)&sa;
+		if (ip) inet_ntop(AF_INET6, (void*)&(s->sin6_addr), ip, ip_len);
+		if (port) *port = ntohs(s->sin6_port);
+	}
+	return fd;
+}
+
+static int _anetTcpServer(char *err, int port, char *bindaddr, int af, int backlog) {
+	int s, rv;
+	char _port[6];
+	struct addrinfo hints;
+	struct addrinfo *servinfo, *p;
+
+	snprintf(_port, 6, "%d", port); /* 将端口的值转换为char类型 */
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = af;
+	hints.ai_socktype = SOCK_STREAM; /* 流式套接字 */
+	//hints.ai_flags = AI_PASSIVE;
+
+	if ((rv = getaddrinfo(bindaddr, _port, &hints, &servinfo)) != 0) {
+		//anetSetError(err, "%s", gai_sererror(rv));
+		return ANET_ERR;
+	}
+
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((s = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+			continue; /* 创建套接字失败的话,继续 */
+		if (anetSetReuseAddr(err, s) == ANET_ERR) goto error;
+		if (anetListen(err, s, p->ai_addr, p->ai_addrlen, backlog) == ANET_ERR) goto error;
+
+		goto end;
+	}
+	if (p == NULL) {
+		anetSetError(err, "unable to bind socket");
+		goto error;
+	}
+error:
+	s = ANET_ERR;
+end:
+	freeaddrinfo(servinfo);
+	return s;
+}
+
+int anetTcpServer(char *err, int port, char *bindaddr, int backlog) {
+	return _anetTcpServer(err, port, bindaddr, AF_INET, backlog);
+}
+
+/*
+* 将 fd 设置为非阻塞模式 (O_NONBLOCK)
+*/
+int anetNonBlock(char *err, int fd) {
+	int flags;
+
+	if ((flags = fcntl(fd, F_GETFL)) == -1) { /* 首先获得原本在fd上的一些特性 */
+		anetSetError(err, "fcntl(F_GETFL): %s", strerror(errno));
+		return ANET_ERR;
+	}
+	if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) { /* 然后添上O_NONBLOCK这个属性 */
+		anetSetError(err, "fcntl(F_SETFL,O_NONBLOCK): %s", strerror(errno));
+		return ANET_ERR;
+	}
+	return ANET_OK;
+}
+
+
+/*
+* 打开或者关闭 Nagle 算法
+*/
+
+static int anetSetTcpNoDelay(char *err, int fd, int val) {
+	if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &val, sizeof(val)) == -1) {
+		anetSetError(err, "setsockopt TCP_NODELAY: %s", strerror(errno));
+		return ANET_ERR;
+	}
+	return ANET_OK;
+}
+
+
+/*
+* 禁用 Nagle 算法
+*/
+int anetEnableTcpNoDelay(char *err, int fd) {
+	return anetSetTcpNoDelay(err, fd, 1);
+}
+
+/*
+* 修改 TCP 连接的 keepalive选项
+*/
+int anetKeepAlive(char *err, int fd, int interval) {
+	int val = 1;
+	if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &val, sizeof(val)) == -1) {
+		anetSetError(err, "setsockopt SO_KEEPALIVE: %s", strerror(errno));
+		return ANET_ERR;
+	}
+	return ANET_OK;
+}
---
+++ 03-finished_string_command/util.c
@@ -0,0 +1,135 @@
+#include "fmacros.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <float.h>
+#include "util.h"
+
+#define LOG_SIZE 1024
+
+/*
+* 一个简易的日志函数.
+*/
+
+void mlog(const char *fileName, int lineNum, const char *func, const char *log_str, ...) {
+	va_list vArgList; /*定义一个va_list型的变量,这个变量是指向参数的指针. */
+	char buf[LOG_SIZE];
+	va_start(vArgList, log_str); /* 用va_start宏初始化变量,这个宏的第二个参数是第一个可变参数的前一个参数,是一个固定的参数 */
+	vsnprintf(buf, LOG_SIZE, log_str, vArgList); /* 注意,不要漏掉前面的_ */
+	va_end(vArgList);  /* 用va_end宏结束可变参数的获取 */
+	printf("%s:%d:%s --> %s\n", fileName, lineNum, func, buf);
+}
+
+/* 将一个字符串类型的数转换为longlong类型.
+* 如果可以转换的话,返回1,否则的话,返回0
+*/
+int string2ll(const char *s, size_t slen, long long *value) {
+	const char *p = s;
+	size_t plen = 0;
+	int negative = 0;
+	unsigned long long v;
+
+	if (plen == slen)
+		return 0;
+
+	/* Special case: first and only digit is 0. */
+	if (slen == 1 && p[0] == '0') {
+		if (value != NULL) *value = 0;
+		return 1;
+	}
+
+	if (p[0] == '-') {
+		negative = 1;
+		p++; plen++;
+
+		/* Abort on only a negative sign. */
+		if (plen == slen)
+			return 0;
+	}
+
+	/* First digit should be 1-9, otherwise the string should just be 0. */
+	if (p[0] >= '1' && p[0] <= '9') {
+		v = p[0] - '0';
+		p++; plen++;
+	}
+	else if (p[0] == '0' && slen == 1) {
+		*value = 0;
+		return 1;
+	}
+	else {
+		return 0;
+	}
+
+	while (plen < slen && p[0] >= '0' && p[0] <= '9') {
+		if (v >(ULLONG_MAX / 10)) /* Overflow. */
+			return 0;
+		v *= 10;
+
+		if (v > (ULLONG_MAX - (p[0] - '0'))) /* Overflow. */
+			return 0;
+		v += p[0] - '0';
+
+		p++; plen++;
+	}
+
+	/* Return if not all bytes were used. */
+	if (plen < slen)
+		return 0;
+
+	if (negative) {
+		if (v >((unsigned long long)(-(LLONG_MIN + 1)) + 1)) /* Overflow. */
+			return 0;
+		if (value != NULL) *value = -v;
+	}
+	else {
+		if (v > LLONG_MAX) /* Overflow. */
+			return 0;
+		if (value != NULL) *value = v;
+	}
+	return 1;
+}
+
+/* Convert a long long into a string. Returns the number of
+* characters needed to represent the number, that can be shorter if passed
+* buffer length is not enough to store the whole number. */
+int ll2string(char *s, size_t len, long long value) {
+	char buf[32], *p;
+	unsigned long long v;
+	size_t l;
+
+	if (len == 0) return 0;
+	v = (value < 0) ? -value : value;
+	p = buf + 31; /* point to the last character */
+	do {
+		*p-- = '0' + (v % 10);
+		v /= 10;
+	} while (v);
+	if (value < 0) *p-- = '-';
+	p++;
+	l = 32 - (p - buf);
+	if (l + 1 > len) l = len - 1; /* Make sure it fits, including the nul term */
+	memcpy(s, p, l);
+	s[l] = '\0';
+	return l;
+}
+
+/* Convert a string into a long. Returns 1 if the string could be parsed into a
+* (non-overflowing) long, 0 otherwise. The value will be set to the parsed
+* value when appropriate. */
+int string2l(const char *s, size_t slen, long *lval) {
+	long long llval;
+
+	if (!string2ll(s, slen, &llval))
+		return 0;
+
+	if (llval < LONG_MIN || llval > LONG_MAX)
+		return 0;
+
+	*lval = (long)llval;
+	return 1;
+}
